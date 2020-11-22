89157d9ae952634d6c8870bf2ba7e6613c66c901
Diego Torres <diegotc86@gmail.com>
2020-11-22 13:26:15 -0500

    Add presence validation for transaction_type on category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,10 @@
 class Category < ApplicationRecord
   enum transaction_type: { expense: 0, income: 1 }
+
   has_many :transactions, dependent: :destroy
+
   belongs_to :user
 
   validates :name, presence: true, uniqueness: { scope: %i[user transaction_type] }
+  validates :transaction_type, presence: true
 end
